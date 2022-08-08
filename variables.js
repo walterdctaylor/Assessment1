91e1a97b2c4ce67c372ad1e5863da91bf4b5e9fe
Walter Taylor <walterdctaylor@yahoo.com>
2022-08-08 15:43:27 -0600

    assessment1

---
+++ variables.js
@@ -0,0 +1,40 @@
+//////////////////PROBLEM 1////////////////////
+/* Create a variable called 'myName' with a value that is your name as a string. */
+
+var myName = "Walter";
+
+
+//////////////////PROBLEM 2////////////////////
+/* Create a variable called 'faveNum' with a value that is your favorite number
+
+int faveNum = 21;
+
+
+//////////////////PROBLEM 3////////////////////
+/* Create a variable called 'lovesCode' and assign it a boolean value. */
+
+var X = ("lovesCode") is true
+
+
+//////////////////PROBLEM 4////////////////////
+/*
+  Write an if-statement that checks if the faveNum variable is 
+  equal to 13. If it is, console.log('lucky 13!'). 
+  If it's not, console.log('not lucky 13').
+*/
+
+var f = 21
+if ('21 = 13'){
+  console.log('not lucky 13.')
+}
+
+//////////////////PROBLEM 5////////////////////
+/*
+  Write a for loop that prints out your faveNum 5 times.
+*/
+
+for (let i = 0; < 5; i+++){
+  console.log(myFaveNum21)
+}
+
+
